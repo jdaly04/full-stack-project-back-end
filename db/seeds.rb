25a72465457197e47af4ed3e04bb5d522b71d0a0
jdaly04 <julia.m.daly@gmail.com>
2016-12-05 15:52:09 -0500

    Update master with seed data
    Update with fundraiser seed data uncommented.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,55 +1,51 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database
-# with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the
-# db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# fundraisers = Fundraiser.create([{
-#   name: 'BGCB House Party',
-#   description: 'House party to support Boys & Girls Clubs of Boston',
-#   url: 'http://www.bgcb.org/2016-house-party/',
-#   city: 'Boston'
-# },
-#
-# {
-#   name: 'Party in the Park',
-#   description: 'Luncheon at Jamaica Pond for park preservation',
-#   url: 'http://www.emeraldnecklace.org/party-in-the-park/',
-#   city: 'Boston'
-# },
-#
-# {
-#   name: 'Boston SantaCon',
-#   description: 'Bar crawl',
-#   url: 'https://www.santacon.org/event/boston-santacon/',
-#   city: 'Boston'
-# },
-#
-# {
-#   name: 'Boston SantaCon',
-#   description: 'Bar crawl',
-#   url: 'https://www.santacon.org/event/boston-santacon/',
-#   city: 'Boston'
-# },
-#
-# {
-#   name: 'CHARITY EVENT JACMEL HAITI, ART SALE',
-#   description: 'art sale to benefit Haitian hurricane victims',
-#   url: 'https://www.eventbrite.com/e/charity-event-jacmel-haiti-art-sale-tickets-29783871361?aff=es2',
-#   city: 'New York'
-# },
-#
-# {
-#   name: 'Grand Holiday Bazaar NYC',
-#   description: 'weekly market in December',
-#   url: 'http://newyorkcity.eventful.com/events/grand-holiday-bazaar-nyc-/E0-001-093933688-2@2016120310',
-#   city: 'New York'
-# }])
+# Examples:
+
+#  cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#  Mayor.create(name: 'Emanuel', city: cities.first)
+
+fundraisers = Fundraiser.create([{
+  name: 'BGCB House Party',
+  description: 'House party to support Boys & Girls Clubs of Boston',
+  url: 'http://www.bgcb.org/2016-house-party/',
+  city: 'Boston'
+},
+
+{
+  name: 'Party in the Park',
+  description: 'Luncheon at Jamaica Pond for park preservation',
+  url: 'http://www.emeraldnecklace.org/party-in-the-park/',
+  city: 'Boston'
+},
+
+{
+  name: 'Boston SantaCon',
+  description: 'Bar crawl',
+  url: 'https://www.santacon.org/event/boston-santacon/',
+  city: 'Boston'
+},
+
+{
+  name: 'Boston SantaCon',
+  description: 'Bar crawl',
+  url: 'https://www.santacon.org/event/boston-santacon/',
+  city: 'Boston'
+},
+
+{
+  name: 'CHARITY EVENT JACMEL HAITI, ART SALE',
+  description: 'art sale to benefit Haitian hurricane victims',
+  url: 'https://www.eventbrite.com/e/charity-event-jacmel-haiti-art-sale-tickets-29783871361?aff=es2',
+  city: 'New York'
+},
+
+{
+  name: 'Grand Holiday Bazaar NYC',
+  description: 'weekly market in December',
+  url: 'http://newyorkcity.eventful.com/events/grand-holiday-bazaar-nyc-/E0-001-093933688-2@2016120310',
+  city: 'New York'
+}])
 
 libraries = Library.create([{
   name: 'My Fundraisers YAY'
