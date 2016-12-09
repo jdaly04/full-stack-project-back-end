8b1db4a11a6d134e51a988490579a1e0462acf45
jdaly04 <julia.m.daly@gmail.com>
2016-12-09 14:31:43 -0500

    Add 'get' all libraries route
    Update with functioning get all libraries
    route.

--- scripts/change-password.sh
+++ scripts/change-password.sh
@@ -14,18 +14,18 @@ curl "${API}${URL_PATH}/${ID}" \
     }
   }'
 
-  API="http://localhost:4741"
-  URL_PATH="/libraries"
-  curl "${API}${URL_PATH}/${ID}" \
-    --include \
-    --request PATCH \
-    --header "Authorization: Token token=${TOKEN}" \
-    --header "Content-Type: application/json" \
-    --data '{
-      "library": {
-        "id": "59",
-        "name": "Floral"
-      }
-    }'
+  # API="http://localhost:4741"
+  # URL_PATH="/libraries"
+  # curl "${API}${URL_PATH}/59" \
+  #   --include \
+  #   --request PATCH \
+  #   --header "Authorization: Token token=BAhJIiU1NzgyNjI2ZmZhNmM5MGFjMTNlNTBkZDBjZjVhYWU1MQY6BkVG--ccb588928d42319b7dac2a8e6614624457eb6859" \
+  #   --header "Content-Type: application/json" \
+  #   --data '{
+  #     "library": {
+  #       "id": "59",
+  #       "name": "Floral"
+  #     }
+  #   }'
 
 echo
